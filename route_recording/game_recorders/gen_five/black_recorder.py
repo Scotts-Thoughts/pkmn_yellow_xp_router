768c56288ce2bab86357886991b58c4d5dc1a5c5
Scotts-Thoughts <99685188+Scotts-Thoughts@users.noreply.github.com>
2025-12-28 15:03:51 -0700

    Revert "reverted major commit"
    This reverts commit f8dcfd77e44c95efcf6f1cc6c195c751563918df.

--- route_recording/game_recorders/gen_five/black_recorder.py
+++ route_recording/game_recorders/gen_five/black_recorder.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 import logging
 from typing import List
-from route_recording.game_recorders.gen_four.platinum_fsm import Machine
+from route_recording.game_recorders.gen_five.platinum_fsm import Machine
 
 import route_recording.recorder
-from route_recording.game_recorders.gen_four import platinum_states
-from route_recording.game_recorders.gen_four.platinum_gamehook_constants import gh_gen_four_const, GameHookConstantConverter
+from route_recording.game_recorders.gen_five import platinum_states
+from route_recording.game_recorders.gen_five.platinum_gamehook_constants import gh_gen_five_const, GameHookConstantConverter
 from utils.constants import const
 
 logger = logging.getLogger(__name__)
@@ -16,9 +16,9 @@ class PlatinumRecorder(route_recording.recorder.RecorderGameHookClient):
         super().__init__(controller, expected_names)
 
         if is_hgss:
-            gh_gen_four_const.configure_for_heartgold()
+            gh_gen_five_const.configure_for_black2()
         else:
-            gh_gen_four_const.configure_for_platinum()
+            gh_gen_five_const.configure_for_black()
 
         self._machine = Machine(controller, self, GameHookConstantConverter(), is_hgss=is_hgss)
 
@@ -29,19 +29,19 @@ class PlatinumRecorder(route_recording.recorder.RecorderGameHookClient):
         self._machine.register(platinum_states.UninitializedState(self._machine))
         self._machine.register(platinum_states.ResettingState(self._machine))
         self._machine.register(platinum_states.BattleState(self._machine))
-        # self._machine.register(platinum_states.InventoryChangeState(self._machine))
-        # self._machine.register(platinum_states.UseRareCandyState(self._machine))
-        # self._machine.register(platinum_states.UseTMState(self._machine))
-        # self._machine.register(platinum_states.MoveDeleteState(self._machine))
-        # self._machine.register(platinum_states.UseVitaminState(self._machine))
+        self._machine.register(platinum_states.InventoryChangeState(self._machine))
+        self._machine.register(platinum_states.UseRareCandyState(self._machine))
+        self._machine.register(platinum_states.UseTMState(self._machine))
+        self._machine.register(platinum_states.MoveDeleteState(self._machine))
+        self._machine.register(platinum_states.UseVitaminState(self._machine))
         self._machine.register(platinum_states.OverworldState(self._machine))
     
     def on_mapper_loaded(self):
         result = super().on_mapper_loaded()
 
         if self._controller.is_ready():
-            self.validate_constants(gh_gen_four_const)
-            for cur_key in gh_gen_four_const.ALL_KEYS_TO_REGISTER:
+            self.validate_constants(gh_gen_five_const)
+            for cur_key in gh_gen_five_const.ALL_KEYS_TO_REGISTER:
                 self.get(cur_key).change(self._machine.handle_event)
 
             if not self._machine._active:
