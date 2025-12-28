bed4838d81327f07731362d5a22d8855a6e6b83e
Scotts-Thoughts <99685188+Scotts-Thoughts@users.noreply.github.com>
2025-12-28 14:50:06 -0700

    changes to build script to output date

--- utils/constants.py
+++ utils/constants.py
@@ -5,8 +5,9 @@ import sys
 
 class Constants:
     def __init__(self):
+        self.DEVELOPMENT_MODE = True
         self.APP_VERSION = "v4.0a"
-        self.APP_RELEASE_DATE = "2025-Dec-27"
+        self.APP_RELEASE_DATE = "2025-Dec-28"
 
         self.DEBUG_MODE = False
         self.DEBUG_RECORDING_MODE = False
